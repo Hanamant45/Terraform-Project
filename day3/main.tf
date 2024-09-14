b0a9fe6fbc15c52ff14c62d416bad1bf024d011c
Hanamant <hmane225@gmail.com>
2024-09-15 00:15:13 +0530

    day4

--- day3/main.tf
+++ day3/main.tf
@@ -1,7 +1,14 @@
-resource "aws_s3_bucket" "name" {
+resource "aws_s3_bucket" "bucket" {
     bucket = "hanamnatmane"
     tags = {
       Name = "HM"
     }
   
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
