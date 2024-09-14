8624cbe3517bef2b400ec68db772cc1821897dc8
Hanamant45 <hmane225@gmail.com>
2024-09-14 18:10:03 +0530

    Update main.tf

--- day4_RDS/main.tf
+++ day4_RDS/main.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "rds" {
   engine               = "mysql"
   engine_version       = "8.0.35"
   instance_class       = "db.t3.micro"
-  username             = "hanamant"
-  password             = ""
+  user             = "hanamant"
+  pwd             = ""
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 }
